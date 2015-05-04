50c263b2a3ac41302d196b55ee82f8d87402493b
Bill Schofield <bill@billschofield.net>
2015-05-04 09:26:42 -0500

    Removed target directory from version control. Inlined some methods in main

--- src/main/java/com/thoughtworks/springinjection/App.java
+++ src/main/java/com/thoughtworks/springinjection/App.java
@@ -12,33 +12,31 @@ public class App {
     public static void main(String[] args) {
         final ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        nonInjectedWay();
-        injectedWay();
-        springXmlWay(context);
-        springAutowiredWay(context);
-    }
+        {
+            Vehicle vehicle = new NonInjectedVehicle();
+            System.out.println(vehicle.go());
+        }
 
-    private static void nonInjectedWay() {
-        Vehicle vehicle = new NonInjectedVehicle();
-        System.out.println(vehicle.go());
-    }
+        {
+            SquirtGun weapon = new SquirtGun();
+            WheeledMover mover = new WheeledMover();
+            final Vehicle vehicle = new ManuallyInjectedVehicle(weapon, mover);
+            System.out.println(vehicle.go());
+        }
 
-    private static void injectedWay() {
-        final Vehicle vehicle = new ManuallyInjectedVehicle(new SquirtGun(), new WheeledMover());
-        System.out.println(vehicle.go());
-    }
+        {
+            Vehicle vehicle = (Vehicle) context.getBean("vehicle");
+            System.out.println(vehicle.go());
 
-    private static void springXmlWay(ApplicationContext context) {
-        Vehicle vehicle = (Vehicle) context.getBean("vehicle");
-        System.out.println(vehicle.go());
 //        Vehicle flyingSquirtGun = (Vehicle) context.getBean("squirtGunAirplane");
 //        System.out.println(flyingSquirtGun.go());
 //        Vehicle autowiredVehicle = (Vehicle) context.getBean("autowiredVehicle");
 //        System.out.println(autowiredVehicle.go());
-    }
+        }
 
-    private static void springAutowiredWay(ApplicationContext context) {
-        Vehicle vehicle = context.getBean(AutowiredVehicle.class);
-        System.out.println(vehicle.go());
+        {
+            Vehicle vehicle = context.getBean(AutowiredVehicle.class);
+            System.out.println(vehicle.go());
+        }
     }
 }
